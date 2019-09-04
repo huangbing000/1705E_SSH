fca984a83f379c3088698aeba66cdb1e4b66d429
huangbing <542619252@qq.com>
2019-09-04 19:48:55 +0800

    第一次提交

---
+++ haungbing-exam5/src/main/java/com/user/User.java
@@ -0,0 +1,25 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: User.java 
+ * @Prject: haungbing-exam5
+ * @Package: com.user 
+ * @Description: TODO
+ * @author: lenovo   
+ * @date: 2019年9月4日 下午7:42:47 
+ * @version: V1.0   
+ */
+package com.user;
+
+/** 
+ * @ClassName: User 
+ * @Description: TODO
+ * @author: lenovo
+ * @date: 2019年9月4日 下午7:42:47  
+ */
+public class User {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+}
